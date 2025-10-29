8b25e6ac5562f5a27cb9b1bc565b52ba74670305
Jun <ljun3380@gmail.com>
2025-10-29 18:09:13 -0400

    Updated file Changes:  - Updated "Main.java"

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main {
         Dog myDog = new Dog("Buddy", 3, "Golden Retriever");
         myDog.displayInfo(); // Displays: Name: Buddy, Age: 3, Breed: Golden Retriever
         myDog.makeSound(); // Displays: Buddy says: Woof!
+
+        BoneEatingWorm myEvilWorm = new BoneEatingWorm("Quincy", 2, 2.5f);
+        myEvilWorm.displayInfo();
+        myEvilWorm.eatBone();
     }
 }
